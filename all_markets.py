4a3505d2b80fd7e11768a29a3c913f99da96e59d
rui sp <manuelsapereira@gmail.com>
2022-05-12 17:44:48 +0800

    Added news and historical charts.

--- all_markets.py
+++ all_markets.py
@@ -1,30 +1,56 @@
 from dash import Dash, dash_table, dcc, html, Input, Output
-
+import dash
+import pandas as pd
 # A bit of style
 import dash_bootstrap_components as dbc
-from aux import get_names_symbols, get_all_quotes
+from fmp_extractor.news.news import extract_top_news
+
+from aux import get_names_symbols, get_all_quotes, graph_callback_all_history
 
 # List of options for tickers based on the list of available symbols
 names_symbols = get_names_symbols()
 exchanges = names_symbols.exchange.unique()
 
+# Values to compute the averages of the means
+SHORT_TERM = 30
+LONG_TERM = 200
+
 # Order of the columns to show in the table
-order_column = ['name', 'symbol', 'changesPercentage', 'marketCap', 'volume', 'voltoavgvolume',  'price',  'change', 'dayLow',
-                      'dayHigh', 'yearHigh', 'yearLow',   'priceAvg50',
-                      'priceAvg200', 'exchange', 'open',
-                      'previousClose', 'eps', 'pe', 'earningsAnnouncement',
-                      'sharesOutstanding', 'timestamp']
+order_column = ['name', 'symbol', 'price', 'changesPercentage', 'price_to_yearHighpercent', 'marketCap', 'volume',
+                'voltoavgvolume', 'change', 'dayLow',
+                'dayHigh', 'yearHigh', 'yearLow',
+                'priceAvg200', 'exchange', 'open',
+                'previousClose', 'eps', 'pe', 'earningsAnnouncement',
+                'sharesOutstanding', 'timestamp']
 app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
-app.layout = html.Div(children=[
-    html.H4("Market view"),
-    dcc.Dropdown(
-        id='exchange',
-        value='XETRA',
-        clearable=True,
-        options=exchanges
-    ),
-    html.Div(id='table')
-])
+app.layout = html.Div(className='row', children=[
+    html.Div(children=[
+        html.H4("Market view"),
+        dcc.Dropdown(
+            id='exchange',
+            value='XETRA',
+            clearable=True,
+            options=exchanges
+        ),
+        html.Div(id='table')]),
+    html.Div(className='row', children=[ html.Div(className='row', children=[
+        html.H4("Historical Chart"),
+        dcc.Dropdown(
+            id='name_1',
+            value='Netflix, Inc., NFLX',
+            clearable=True,
+            options=names_symbols.index
+        ),
+        dcc.Graph(id='close_price_1')], style={'display': 'inline-block', 'width': '48%', 'height': '50%'}),
+                                        html.Div(className='row', children=[
+                                            html.H4("Latest News"),
+                                            html.Div(children=html.Div(id='no_news', children=[],
+                                                                       style={'width': '3', 'height': '15'})),
+                                            html.Div(className='row', children=[html.Div(id='news')])],
+                                                 style={'display': 'inline-block', 'width': '48%', 'height': '700'}
+                                                 )]
+             )]
+)
 
 
 @app.callback(Output('table', 'children'), Input('exchange', 'value'))
@@ -50,8 +76,8 @@ def get_quotes_exchange(exch):
         selected_columns=[],
         selected_rows=[],
         page_action="native",
-        page_current= 0,
-        page_size= 20,
+        page_current=0,
+        page_size=10,
         style_header={
             'backgroundColor': 'rgb(30, 30, 30)',
             'color': 'white',
@@ -66,6 +92,78 @@ def get_quotes_exchange(exch):
     ),
 
 
+@app.callback(Output('close_price_1', 'figure'), Input('name_1', 'value'))
+def price_hist_1(name_1):
+    close_figure = graph_callback_all_history(name_1, names_symbols, freq=None, short_term=SHORT_TERM,
+                                              long_term=LONG_TERM)
+    close_figure.update_layout(height=700, yaxis=dict(autorange=True, fixedrange=False),
+                               xaxis=dict(
+                                   rangeselector=dict(
+                                       buttons=list([
+                                           dict(count=1,
+                                                label="1m",
+                                                step="month",
+                                                stepmode="backward"),
+                                           dict(count=6,
+                                                label="6m",
+                                                step="month",
+                                                stepmode="backward"),
+                                           dict(count=1,
+                                                label="YTD",
+                                                step="year",
+                                                stepmode="todate"),
+                                           dict(count=1,
+                                                label="1y",
+                                                step="year",
+                                                stepmode="backward"),
+                                           dict(count=2,
+                                                label="2y",
+                                                step="year",
+                                                stepmode="backward"),
+                                           dict(count=5,
+                                                label="5y",
+                                                step="year",
+                                                stepmode="backward"),
+                                           dict(step="all")
+                                       ])
+                                   ),
+                                   rangeslider=dict(
+                                       visible=True
+                                   ),
+                                   type="date"
+                               )
+                               )
+    return close_figure
+
+
+@app.callback(Output('news', 'children'),
+              Output('no_news', 'children'),
+              Input('name_1', 'value'))
+def generate_table(name):
+    ticker = names_symbols.at[name, 'symbol']
+    news = extract_top_news([ticker], limit=50)
+    try:
+        df = pd.DataFrame.from_records(list(news.values())[0]).loc[:, ['publishedDate', 'title', 'text', 'url']]
+    except KeyError:
+        return dash.no_update, dbc.Alert('No news for the selected ticker', color='danger', dismissable=True)
+
+    df['Date'] = df['publishedDate'].str[:11]
+    df['Time'] = df['publishedDate'].str[11:]
+    table = html.Table([
+        html.Thead(
+            html.Tr([html.Th(col) for col in ['Date', 'Time', 'Title']],
+                    style={'white-space': 'nowrap', 'padding': '3px'})
+        ),
+        html.Tbody([
+            html.Tr([
+                html.Td(df.iloc[i]['Date'], style={'white-space': 'nowrap', 'padding': '3px'}),
+                html.Td(df.iloc[i]['Time'], style={'white-space': 'nowrap', 'padding': '3px'}),
+                dcc.Link(html.A(df.iloc[i]['title']), href=df.iloc[i]['url'], target="_blank")
+            ]) for i in range(df.shape[0])])
+    ])
+    return dbc.Table(table, bordered=True, striped=True, responsive=True, color='light',
+                     style={'white-space': 'nowrap', 'border-spacing': '3px'}), dash.no_update
+
+
 if __name__ == '__main__':
     app.run_server(debug=True, port=8070)
-
